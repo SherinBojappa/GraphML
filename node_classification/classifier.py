d426d6d01e5d68580203c0a17154e0f6560d3edf
Sherin Bojappa Muckatira <sherin2013007@gmail.com>
2021-10-30 10:45:24 -0400

    add code to read train, test, and val data

--- node_classification/classifier.py
+++ node_classification/classifier.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import scipy
 
 
-def plot_graph(G, num_nodes):
+def plot_graph(G):
     """
     :param G:
     :param num_nodes:
@@ -18,17 +18,33 @@ def main(args):
     # read the nodes
     G = nx.read_edgelist(args.network_file, nodetype=int)
     print(nx.info(G))
+    #TODO remap the nodes to ones starting from 0
+    # the nodes are already in sorted order 0 - something
+    print(sorted(G.nodes()))
 
     # convert the network file into a pandas dataframe
     hyperlinks = pd.read_csv(args.network_file, sep=' ',
                              names=["source", "target"])
-    #print(hyperlinks.head())
+    print("Hyperlinks")
+    print(hyperlinks.head())
 
-    classes = pd.read_csv(args.categories, sep=' ',
-                          names=["class_label", "category"])
-    #print(classes.head())
+    class_labels = pd.read_csv(args.categories, sep=' ',
+                          names=["class_label_id", "category"])
+    print(class_labels.head())
+    print("class labels")
 
-    # create the training dataset
+    # read the training, validation, test data
+    train_data = pd.read_csv(args.train, sep=' ', names=["node", "class_label"])
+    print("train data")
+    print(train_data.head())
+
+    val_data = pd.read_csv(args.val, sep=' ', names=["node", "class_label"])
+    print("val data")
+    print(val_data.head())
+
+    test_data = pd.read_csv(args.test, names=["node"])
+    print("test data")
+    print(test_data.head())
 
     # plot sample graph
     # plot_graph(G, num_nodes)
