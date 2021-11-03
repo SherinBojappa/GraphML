2b1c0c5687990ddc22c57da776451d6be242e950
Sherin Bojappa Muckatira <sherin2013007@gmail.com>
2021-11-02 21:15:50 -0400

    GNN

--- node_classification/classifier.py
+++ node_classification/classifier.py
@@ -11,13 +11,217 @@ import scipy
 import numpy as np
 
 
-def plot_graph(G):
-    """
-    :param G:
-    :param num_nodes:
-    :return:
-    """
-    # plot graph
+def extract_graph_features(G, node_to_vec):
+    # (2, numedges) - (source, target)
+    edges = np.array(G.edges).T
+    # Create an edge weights array of ones.
+    edge_weights = tf.ones(shape=edges.shape[1])
+    # Create a node features array of shape [num_nodes, num_features].
+    node_features = tf.cast(np.array(list(node_to_vec.values())), dtype=tf.dtypes.float32)
+    # Create graph info tuple with node_features, edges, and edge_weights.
+    graph_info = (node_features, edges, edge_weights)
+
+    return graph_info
+
+def run_experiment(model, x_train, y_train, x_val, y_val):
+    # Compile the model.
+    model.compile(
+        optimizer=keras.optimizers.Adam(args.learning_rate),
+        loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        metrics=[keras.metrics.SparseCategoricalAccuracy(name="acc")],
+    )
+    # Create an early stopping callback.
+    early_stopping = keras.callbacks.EarlyStopping(
+        monitor="val_acc", patience=50, restore_best_weights=True
+    )
+    # Fit the model.
+    history = model.fit(
+        x=x_train,
+        y=y_train,
+        epochs=args.num_epochs,
+        batch_size=args.batch_size,
+        validation_data=(x_val, y_val),
+        #validation_split=0.15,
+        callbacks=[early_stopping],
+    )
+    return history
+
+def create_ffn(hidden_units, dropout_rate, name=None):
+    fnn_layers = []
+    for units in hidden_units:
+        fnn_layers.append(layers.BatchNormalization())
+        fnn_layers.append(layers.Dropout(dropout_rate))
+        fnn_layers.append(layers.Dense(units, activation=tf.nn.gelu))
+    return keras.Sequential(fnn_layers, name=name)
+
+
+class GraphConvLayer(layers.Layer):
+    def __init__(
+            self,
+            hidden_units,
+            dropout_rate=0.2,
+            aggregation_type="mean",
+            combination_type="concat",
+            normalize=False,
+            *args,
+            **kwargs,
+    ):
+        super(GraphConvLayer, self).__init__(*args, **kwargs)
+        self.aggregation_type = aggregation_type
+        self.combination_type = combination_type
+        self.normalize = normalize
+        self.ffn_prepare = create_ffn(hidden_units, dropout_rate)
+        if self.combination_type == "gated":
+            self.update_fn = layers.GRU(
+                units=hidden_units,
+                activation="tanh",
+                recurrent_activation="sigmoid",
+                dropout=dropout_rate,
+                return_state=True,
+                recurrent_dropout=dropout_rate,
+            )
+        else:
+            self.update_fn = create_ffn(hidden_units, dropout_rate)
+
+    def prepare(self, node_repesentations, weights=None):
+        # node_repesentations shape is [num_edges, embedding_dim].
+        messages = self.ffn_prepare(node_repesentations)
+        if weights is not None:
+            messages = messages * tf.expand_dims(weights, -1)
+        return messages
+
+    def aggregate(self, node_indices, neighbour_messages):
+        # node_indices shape is [num_edges].
+        # neighbour_messages shape: [num_edges, representation_dim].
+        num_nodes = tf.math.reduce_max(node_indices) + 1
+        if self.aggregation_type == "sum":
+            aggregated_message = tf.math.unsorted_segment_sum(
+                neighbour_messages, node_indices, num_segments=num_nodes
+            )
+        elif self.aggregation_type == "mean":
+            aggregated_message = tf.math.unsorted_segment_mean(
+                neighbour_messages, node_indices, num_segments=num_nodes
+            )
+        elif self.aggregation_type == "max":
+            aggregated_message = tf.math.unsorted_segment_max(
+                neighbour_messages, node_indices, num_segments=num_nodes
+            )
+        else:
+            raise ValueError(
+                f"Invalid aggregation type: {self.aggregation_type}.")
+        return aggregated_message
+
+    def update(self, node_repesentations, aggregated_messages):
+        # node_repesentations shape is [num_nodes, representation_dim].
+        # aggregated_messages shape is [num_nodes, representation_dim].
+        if self.combination_type == "gru":
+            # Create a sequence of two elements for the GRU layer.
+            h = tf.stack([node_repesentations, aggregated_messages], axis=1)
+        elif self.combination_type == "concat":
+            # Concatenate the node_repesentations and aggregated_messages.
+            h = tf.concat([node_repesentations, aggregated_messages], axis=1)
+        elif self.combination_type == "add":
+            # Add node_repesentations and aggregated_messages.
+            h = node_repesentations + aggregated_messages
+        else:
+            raise ValueError(
+                f"Invalid combination type: {self.combination_type}.")
+
+        # Apply the processing function.
+        node_embeddings = self.update_fn(h)
+        if self.combination_type == "gru":
+            node_embeddings = tf.unstack(node_embeddings, axis=1)[-1]
+        if self.normalize:
+            node_embeddings = tf.nn.l2_normalize(node_embeddings, axis=-1)
+        return node_embeddings
+
+    def call(self, inputs):
+        """Process the inputs to produce the node_embeddings.
+        inputs: a tuple of three elements: node_repesentations, edges, edge_weights.
+        Returns: node_embeddings of shape [num_nodes, representation_dim].
+        """
+        node_repesentations, edges, edge_weights = inputs
+        # Get node_indices (source) and neighbour_indices (target) from edges.
+        node_indices, neighbour_indices = edges[0], edges[1]
+        # neighbour_repesentations shape is [num_edges, representation_dim].
+        neighbour_repesentations = tf.gather(node_repesentations,
+                                             neighbour_indices)
+
+        # Prepare the messages of the neighbours.
+        neighbour_messages = self.prepare(neighbour_repesentations,
+                                          edge_weights)
+        # Aggregate the neighbour messages.
+        aggregated_messages = self.aggregate(node_indices, neighbour_messages)
+        # Update the node embedding with the neighbour messages.
+        return self.update(node_repesentations, aggregated_messages)
+
+class GNNNodeClassifier(tf.keras.Model):
+    def __init__(
+        self,
+        graph_info,
+        num_classes,
+        hidden_units,
+        aggregation_type="sum",
+        combination_type="concat",
+        dropout_rate=0.2,
+        normalize=True,
+        *args,
+        **kwargs,
+    ):
+        super(GNNNodeClassifier, self).__init__(*args, **kwargs)
+
+        # Unpack graph_info to three elements: node_features, edges, and edge_weight.
+        node_features, edges, edge_weights = graph_info
+        self.node_features = node_features
+        self.edges = edges
+        self.edge_weights = edge_weights
+        # Set edge_weights to ones if not provided.
+        if self.edge_weights is None:
+            self.edge_weights = tf.ones(shape=edges.shape[1])
+        # Scale edge_weights to sum to 1.
+        self.edge_weights = self.edge_weights / tf.math.reduce_sum(self.edge_weights)
+
+        # Create a process layer.
+        self.preprocess = create_ffn(hidden_units, dropout_rate, name="preprocess")
+        # Create the first GraphConv layer.
+        self.conv1 = GraphConvLayer(
+            hidden_units,
+            dropout_rate,
+            aggregation_type,
+            combination_type,
+            normalize,
+            name="graph_conv1",
+        )
+        # Create the second GraphConv layer.
+        self.conv2 = GraphConvLayer(
+            hidden_units,
+            dropout_rate,
+            aggregation_type,
+            combination_type,
+            normalize,
+            name="graph_conv2",
+        )
+        # Create a postprocess layer.
+        self.postprocess = create_ffn(hidden_units, dropout_rate, name="postprocess")
+        # Create a compute logits layer.
+        self.compute_logits = layers.Dense(units=num_classes, name="logits")
+    def call(self, input_node_indices):
+        # Preprocess the node_features to produce node representations.
+        x = self.preprocess(self.node_features)
+        # Apply the first graph conv layer.
+        x1 = self.conv1((x, self.edges, self.edge_weights))
+        # Skip connection.
+        x = x1 + x
+        # Apply the second graph conv layer.
+        x2 = self.conv2((x, self.edges, self.edge_weights))
+        # Skip connection.
+        x = x2 + x
+        # Postprocess node embedding.
+        x = self.postprocess(x)
+        # Fetch node embeddings for the input node_indices.
+        node_embeddings = tf.squeeze(tf.gather(x, input_node_indices))
+        # Compute logits
+        return self.compute_logits(node_embeddings)
 
 
 def extract_features():
@@ -123,15 +327,23 @@ def main(args):
     node_vectors = [node_to_vec[node] for node in test_nodes]
     test_feature_vectors = np.array(node_vectors)
 
+    feature_dim = len(node_to_vec[0])
+    num_classes = len(class_labels["class_label_id"].to_list())
+
+    x_train = train_feature_vectors
+    y_train = train_data["class_label"].to_numpy()
+    x_val = val_feature_vectors
+    y_val = val_data["class_label"].to_numpy()
+
+    x_test = test_feature_vectors
 
     if args.model == "MLP":
         hidden_units = 32
-        dropout_rate = 0.2
-        lr = 0.01
-        num_epochs = 300
-        batch_size = 32
-        feature_dim = len(node_to_vec[0])
-        num_classes = len(class_labels["class_label_id"].to_list())
+        dropout_rate = args.dropout_rate
+        lr = args.learning_rate
+        num_epochs = args.epochs
+        batch_size = args.batch_size
+
         input_features = keras.Input(shape=(feature_dim, ))
 
         x = keras.layers.Dense(hidden_units, activation='gelu')(input_features)
@@ -187,9 +399,6 @@ def main(args):
             monitor="val_acc", patience=100, restore_best_weights=True
         )
 
-        x_train = train_feature_vectors
-        y_train = train_data["class_label"].to_numpy()
-
 
         # Fit the model.
         history = model.fit(
@@ -197,8 +406,7 @@ def main(args):
             y=y_train,
             epochs=num_epochs,
             batch_size=batch_size,
-            validation_data=(val_feature_vectors,
-                             val_data["class_label"].to_numpy()),
+            validation_data=(x_val, y_val),
             #validation_split=0.2,
             callbacks=[early_stopping],
         )
@@ -208,7 +416,29 @@ def main(args):
         #    tf.convert_to_tensor(logits)).numpy()
         #display_class_probabilities(probabilities)
 
-    print("End point")
+    if(args.model == "GNN"):
+
+        hidden_units = [32, 32]
+
+        graph_info = extract_graph_features(G, node_to_vec)
+
+        gnn_model = GNNNodeClassifier(
+            graph_info=graph_info,
+            num_classes=num_classes,
+            hidden_units=hidden_units,
+            dropout_rate=args.dropout_rate,
+            name="gnn_model",
+        )
+
+        print("GNN output shape:", gnn_model([1, 5]))
+
+        gnn_model.summary()
+
+        history = run_experiment(gnn_model, x_train, y_train, x_val, y_val)
+
+        #_, test_accuracy = gnn_model.evaluate(x=x_test, y=y_test, verbose=0)
+        #print(f"Test accuracy: {round(test_accuracy * 100, 2)}%")
+
 
 
 
@@ -226,6 +456,11 @@ if __name__ == '__main__':
     parser.add_argument("val", help="validation nodes with category")
     parser.add_argument("test", help="test nodes")
     parser.add_argument("model", default="GNN", help="MLP or GNN")
+    parser.add_argument("num_epochs", default=300, type=int, help="number of epochs")
+    parser.add_argument("batch_size", default=32, type=int, help="samples in a batch")
+    parser.add_argument("learning_rate", default=0.01, type=float, help="learning rate")
+    parser.add_argument("dropout_rate", default=0.2, type=float, help="dropout_rate")
+
     args = parser.parse_args()
 
     main(args)
