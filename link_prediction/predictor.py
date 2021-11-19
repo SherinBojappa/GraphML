0ff0aa5ac35d72796b687c9dfbe20cb6d236ef94
Sherin Bojappa Muckatira <sherin2013007@gmail.com>
2021-11-19 19:53:41 +0530

    read the train and test data

--- link_prediction/predictor.py
+++ link_prediction/predictor.py
@@ -1,4 +1,45 @@
 import argparse
+import networkx as nx
+import pandas as pd
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import matplotlib.pyplot as plt
+import scipy
+import numpy as np
+import gensim
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+import gensim.downloader
+import os
+
+def main(args):
+    train_path = os.path.join(os.getcwd(), "data", "train.txt")
+    G = nx.read_edgelist(train_path, delimiter=',', nodetype=int)
+    print(nx.info(G))
+
+    # convert the network file into a pandas dataframe
+    citation_train_val = pd.read_csv(train_path, sep=',',
+                             names=["source", "target"])
+
+    test_path = os.path.join(os.getcwd(), "data", "test.txt")
+    citation_test = pd.read_csv(test_path, sep=',',
+                             names=["source", "target"])
+
+    print(citation_train_val.head())
+    print("test data")
+    print(citation_test.head())
+
 
 if __name__ == "__main__":
-    print("Link prediction")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", default='MLP', help="GNN or MLP")
+
+    args = parser.parse_args()
+    print(args)
+    main(args)
